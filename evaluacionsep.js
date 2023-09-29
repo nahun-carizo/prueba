5aa34d4b8f3ebb7f38ba193076ea0bd1a7027f73
Nahun Carrizo <44736533+nahun-carizo@users.noreply.github.com>
2023-09-29 19:47:10 -0300

    Add files via upload

---
+++ evaluacionsep.js
@@ -0,0 +1,92 @@
+const http = require('http')
+const fs = require('fs')
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('https://evaluacionsep-566cd.web.app/' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen('evaluacionsep-566cd.web.app/')
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+//////funcion para modificar frase
+    const vocales = "aeiouáéíóúü";
+    let resultado = "";
+    const hola = formulario.get('nombre');
+  
+    for (let i = 0; i < hola.length; i++) {
+      const letra = hola.charAt(i).toLowerCase();
+      if (vocales.includes(letra)) {
+        resultado += letra + "p" + letra;
+      } else {
+        resultado += letra;
+      }
+    }
+//////
+    const pagina =
+      `<!doctype html><html><head></head><body>
+     frase ingresada:${formulario.get('nombre')}<br>
+     traduccion:${resultado}<br>  
+     <a href="index.html">Retornar</a>
+     </body></html>`
+    respuesta.end(pagina)
+  })
+}
+
+console.log('Servidor web iniciado')
